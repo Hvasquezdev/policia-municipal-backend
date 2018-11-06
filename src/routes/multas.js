37fcac6caf1bb73f5ffc1b3a51e8e0261680cf45
Hector Vasquez <hvasquezdev@gmail.com>
2018-11-05 22:23:33 -0400

    Edit multa route implemented

--- src/routes/multas.js
+++ src/routes/multas.js
@@ -8,7 +8,6 @@ router.use(express.json());
 
 // Get all multas
 router.get('/multas', (req, res) => {
-
   mysqlConnection.query('SELECT * FROM multa', (err, results) => {
     if (err) return console.error(err);
     res.json(results);
@@ -18,9 +17,9 @@ router.get('/multas', (req, res) => {
 // Get specific multa
 router.get('/multas/:id', (req, res) => {
   const { id } = req.params; // Parametro recibido por la ruta
-  mysqlConnection.query('SELECT * FROM multa WHERE ID = ?' [id], (err, results) => {
+  const query = 'SELECT * FROM multa WHERE ID = ?';
+  mysqlConnection.query(query, [id], (err, results) => {
     if (err) return console.error(err);
-    console.log(results)
     res.json(results);
   });
 });
@@ -29,12 +28,21 @@ router.get('/multas/:id', (req, res) => {
 router.post('/newMulta', (req, res) => {
   const { nombre, precio, descripcion } = req.body;
   const query = 'INSERT INTO multa (ID, Nombre, Descripcion, Precio) VALUES(NULL, ?, ?, ?);';
-
-  console.log(nombre, precio, descripcion);
-
   mysqlConnection.query(query, [nombre, descripcion, precio], (err, results) => {
     if (err) return console.error(err);
     res.status(200).json(results);
+  });
+});
+
+// Actualiza la multa seleccionada
+router.put('/multas/:id', (req, res) => {
+  const query = 'UPDATE multa SET Nombre = ?, Descripcion = ?, Precio = ? WHERE ID = ?;'
+  const id = req.params.id;
+  const { Nombre, Precio, Descripcion} = req.body;
+
+  mysqlConnection.query(query, [Nombre, Descripcion, Precio, id], (err, results) => {
+    if(err) return console.error(err);
+    res.status(200).json({message: 'Multa actualizada'});
   });
 });
 
